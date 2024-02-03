6c6d11058cefdee5daa4fd48758aa8fa39a30d6c
tszhong0411 <me@honghong.me>
2024-02-03 14:38:15 +0800

    feat: update for easier testing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
-import { type Config } from 'tailwindcss'
+// import { type Config } from 'tailwindcss'
 
-const config: Config = {
+// const config: Config = {
+const config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
         special: '#ff0000'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
 
 export default config
